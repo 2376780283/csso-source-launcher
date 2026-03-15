74a16ef86c252194496b25523e78d69c8432b779
2376780283 <2376780283@qq.com>
2026-03-16 01:55:11 +0800

    修复SDL崩溃

--- app/src/main/java/org/libsdl/app/SDLActivity.java
+++ app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -363,28 +363,6 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
 
 		mClipboardHandler = new SDLClipboardHandler();
 
-		// Enable fullscreen mode and ignore cutout (notch) area
-		if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.P )
-		{
-			// Android 9 (API 28) and above - use display cutout mode
-			getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-		}
-
-		if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.R )
-		{
-			// Android 11 (API 30) and above - use WindowInsetsController
-			getWindow().setDecorFitsSystemWindows( false );
-			getWindow().getInsetsController().hide( android.view.WindowInsets.Type.systemBars() );
-			getWindow().getInsetsController().setSystemBarsBehavior( android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE );
-		}
-		else if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
-		{
-			// Android 4.4 to Android 10 - use legacy fullscreen flags
-			int flags = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
-
-			SDLActivity.this.getWindow().getDecorView().setSystemUiVisibility( flags );
-		}
-
 		// Set up the surface
 		mSurface = new SDLSurface( getApplication() );
 
@@ -412,6 +390,31 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
 		}
 
 		setContentView( mLayout );
+
+		// Enable fullscreen mode and ignore cutout (notch) area
+		if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.P )
+		{
+			// Android 9 (API 28) and above - use display cutout mode
+			getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+		}
+
+		if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.R )
+		{
+			// Android 11 (API 30) and above - use WindowInsetsController
+			getWindow().setDecorFitsSystemWindows( false );
+			if( getWindow().getInsetsController() != null )
+			{
+				getWindow().getInsetsController().hide( android.view.WindowInsets.Type.systemBars() );
+				getWindow().getInsetsController().setSystemBarsBehavior( android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE );
+			}
+		}
+		else if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
+		{
+			// Android 4.4 to Android 10 - use legacy fullscreen flags
+			int flags = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
+
+			SDLActivity.this.getWindow().getDecorView().setSystemUiVisibility( flags );
+		}
 
 		setWindowStyle( false );
 
